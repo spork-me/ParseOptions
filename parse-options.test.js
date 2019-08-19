6cbc082de6207e589e3f78e91b1dc863feb945c3
Jeremy Greer <jex.grizzle@gmail.com>
2019-08-19 07:47:05 -0700

    Poke holes and ruin everyone's fun

--- parse-options.test.js
+++ parse-options.test.js
@@ -9,5 +9,14 @@ describe('parseOptions', () => {
     expect(parseOptions('--red=yes --old=no --what no ---thanks ---wat=ok'))
       .toEqual({ red: 'yes', old: 'no', what: true })
   })
-})
 
+  it('should be resilient to chicanery', () => {
+    expect(parseOptions('--red=yes--=faroutman --old=no --cold========>bang! --spaces="rule over tabs"'))
+      .toEqual({
+        red: 'yes--=faroutman ',
+        old: 'no',
+        cold: '=======>bang!',
+        spaces: 'rule over tabs',
+      })
+  })
+})
